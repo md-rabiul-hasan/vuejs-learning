16249a0ed8945cc82c5169d42d29598ac2f19eee
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 00:11:22 +0600

    lifecycle of vue-js

--- Basic-Topics/app.js
+++ Basic-Topics/app.js
@@ -1,12 +1,38 @@
-Vue.component('name', {
-    data(){
-        return {
-            name: 'Rabiul Hasan'
+new Vue({
+    el: '#app',
+    data: {
+        name: 'Rabiul Hasan'
+    },
+    methods: {
+        updateName(){
+            this.name = "Nazrul Islam"
+        },
+        destroyName(){
+            this.$destroy();
         }
     },
-    template: `<h4>My name is {{ name }}</h4>`
+    beforeCreate() {
+        console.log("before create");
+    },
+    created() {
+        console.log('created');
+    },
+    beforeMount() {
+        console.log('before mount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        console.log('before update')
+    },
+    updated() {
+        console.log('updated')
+    },
+    beforeDestroy() {
+        console.log('before destroy')
+    },
+    destroyed() {
+        console.log('destory')
+    },
 });
-
-new Vue({
-    el: '#app'
-})
