c65e5b59849d04e2d8a34f884ca5a3e3e8a3fffa
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 01:38:30 +0600

    create component & locally import

--- vue-cli-project/src/App.vue
+++ vue-cli-project/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
+    <Header />
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './Components/Header_Footer/Header.vue';
+import Footer from './Components/Header_Footer/Footer.vue';
 export default {
   data(){
     return {
       name: 'Rabiul Hasan',
       age: 13
     }
+  },
+  components:{
+    Header,
+    Footer
   }
 }
 </script>
