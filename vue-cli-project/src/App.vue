866c007db655d75d01496fed2242cd406791e96b
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 01:22:42 +0600

    understanding new project and structure

---
+++ vue-cli-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      name: 'Rabiul Hasan',
+      age: 13
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
