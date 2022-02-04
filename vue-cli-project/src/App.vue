ff97de308bffc3fed4c3841e11e09e6f487196d2
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 03:50:10 +0600

    slot

--- vue-cli-project/src/App.vue
+++ vue-cli-project/src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-    <Header 
-      :students="students"
-      :firstName="firstName"
-      :lastName="lastName"
-      @updateFirstName="firstName = $event"
-    />
-    <Footer />
+    <UserListSlot>
+      <template v-slot:studentList>
+        <ul>
+          <li v-for="(student, index) in students" :key="index">
+            {{ student }}
+          </li>
+        </ul>
+      </template>
+      <template v-slot:loader>
+      </template>
+
+      
+    </UserListSlot>
   </div>
 </template>
 
 <script>
-import Header from './Components/Header_Footer/Header.vue';
-import Footer from './Components/Header_Footer/Footer.vue';
+import UserListSlot from './Components/UserListSlot.vue'
 export default {
-  data(){
-    return {
-      students: ['Rabiul', 'Munna', 'Arif', 'Ramjan'],
-      firstName: 'Rabiul',
-      lastName: 'Hasan'
-    }
-  },
-  components:{
-    Header,
-    Footer
-  }
+ data(){
+   return {
+     students: ['Rabiul', 'Ramjan', 'Halim'],
+     loading : false
+   }   
+ },
+ components:{
+  UserListSlot
+ }
 }
 </script>
 
---
+++ vue-cli-project/src/Components/UserListSlot.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <p>Rabiul Hasan</p>
+        <slot name="studentList"></slot>
+        <slot name="loader">Loading......</slot>
+    </div>
+</template>
