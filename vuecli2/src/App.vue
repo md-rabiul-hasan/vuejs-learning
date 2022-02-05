171edb2c017714cbdf70e619105fa48f228cfa56
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 12:53:41 +0600

    transition

--- vuecli2/src/App.vue
+++ vuecli2/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
-    <p v-user.red.bold="'Rahman Mia'"></p>
-    <p v-people.red.bold="name"></p>
+    <button @click="display=!display">Toggle</button>
+    <hr>
+    <transition name="alert-card">
+      <div v-if="display" class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+    </transition>
+    
   </div>
 </template>
 
@@ -9,25 +15,25 @@
 export default {
   data(){
     return {
-      name: 'Rabiul Hasan'
-    }
-  },
-  directives:{
-    'people': {
-      bind(el,binding){
-        el.innerHTML = binding.value
-        if(binding.modifiers.red){
-          el.style.color='red'
-        }
-        if(binding.modifiers.bold){
-          el.style.fontWeight='bold'
-        }
-      }
+      name: 'Rabiul Hasan',
+      display: false
     }
   }
 }
 </script>
 
 <style>
-  
+  /* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.alert-card-enter-active {
+  transition: all .3s ease;
+}
+.alert-card-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.alert-card-enter, .alert-card-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
