cf624ff54384ae8bf3adbebec59c4670a0327fa6
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 22:31:14 +0600

    vuex setup

---
+++ vuex/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <BookListOne :books="books"/>
+      <BookListTwo :books="books"/>
+    </div>
+</template>
+
+<script>
+import BookListOne from './components/BookListOne.vue'
+import BookListTwo from './components/BookListTwo.vue'
+export default {
+    data(){
+      return {
+        books: [
+          {name: 'Laravel Book', price: 120},
+          {name: 'Devops Book', price: 100},
+          {name: 'Js Book', price: 200},
+          {name: 'Data Structure & Algorithm', price: 150},
+        ]
+      }
+    },
+    components:{
+      BookListOne,
+      BookListTwo
+    }
+}
+</script>
+
+<style lang="">
+  
+</style>
---
+++ vuex/src/components/BookListOne.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="book-list-one">
+      <h4>BookList One</h4>
+      <ul>
+          <li v-for="(book, index) in books" :key="index">
+              <span class="name">{{ book.name }}</span>
+              <span class="name">$ {{ book.price }}</span>
+          </li>
+        </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return {
+
+      }
+  },
+  props: ['books']
+}
+</script>
+
+<style scoped>
+    #book-list-one{
+    background: rgb(218, 201, 14);
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 10px;
+    padding: 10px 20px;
+}
+#book-list-one ul{
+    padding: 0;
+}
+#book-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
+</style>
