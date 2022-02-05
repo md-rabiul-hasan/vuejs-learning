3c06eb1309d2a47965a9f16e13716b009cd1b137
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 12:39:15 +0600

    custom directive

--- vuecli2/src/App.vue
+++ vuecli2/src/App.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="container">
     <p v-user.red.bold="'Rahman Mia'"></p>
+    <p v-people.red.bold="name"></p>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      name: 'Rabiul Hasan'
+    }
+  },
+  directives:{
+    'people': {
+      bind(el,binding){
+        el.innerHTML = binding.value
+        if(binding.modifiers.red){
+          el.style.color='red'
+        }
+        if(binding.modifiers.bold){
+          el.style.fontWeight='bold'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
