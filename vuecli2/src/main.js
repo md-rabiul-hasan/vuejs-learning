41b1c7309d85b9b815edd2ab94b752dd565e2af2
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 12:36:15 +0600

    custom global directive

--- vuecli2/src/main.js
+++ vuecli2/src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('user', {
+  bind(el, binding){
+    el.innerHTML = binding.value
+    if(binding.modifiers.red){
+      el.style.color='red'
+    }
+    if(binding.modifiers.bold){
+      el.style.fontWeight='bold'
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
