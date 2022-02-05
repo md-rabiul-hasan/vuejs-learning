dccb4791671bbd8b3cf7dbfd8ee0fdeca8004fef
rabiulhasan3 <rabiul.fci@gmail.com>
2022-02-05 12:59:43 +0600

    transaction custom classes

--- vuecli2/src/App.vue
+++ vuecli2/src/App.vue
@@ -2,7 +2,11 @@
   <div class="container">
     <button @click="display=!display">Toggle</button>
     <hr>
-    <transition name="alert-card">
+    <transition 
+    name="alert-card"
+    enter-active-class="animated fadeInUp"
+    leave-active-class="animated fadeInDown delay-3s"
+    >
       <div v-if="display" class="alert alert-primary" role="alert">
         A simple primary alert—check it out!
       </div>
@@ -22,18 +26,3 @@ export default {
 }
 </script>
 
-<style>
-  /* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.alert-card-enter-active {
-  transition: all .3s ease;
-}
-.alert-card-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.alert-card-enter, .alert-card-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
-</style>
